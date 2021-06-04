a6c1efa391a41177390b3a91b475a26bc715c050
Andre Biedenkapp <biedenka@informatik.uni-freiburg.de>
2021-06-04 08:56:26 +0200

    Move test

--- test/test_agent/ddqn.py
+++ test/test_agent/ddqn.py
@@ -3,19 +3,17 @@ from unittest.mock import MagicMock
 from unittest.mock import patch
 
 from mighty.agent.ddqn import DDQNAgent
+from mock_environment import MockEnv
 
 
 class MyTestCase(unittest.TestCase):
 
-    # TODO properly mock environment
     @patch('mighty.agent.ddqn.optim.Adam')
-    @patch('mighty.agent.ddqn.FullyConnectedQ')
-    def setUp(self, mocked_fully_connected_q, mocked_optimizer) -> None:
+    def setUp(self, mocked_optimizer) -> None:
+        env = MockEnv
         self.ddqn = DDQNAgent(env=MagicMock(), env_eval=MagicMock(),
                               gamma=.99, epsilon=.1, batch_size=64, logger=MagicMock())
         mocked_optimizer.assert_called_once()
-
-
 
 if __name__ == '__main__':
     unittest.main()
