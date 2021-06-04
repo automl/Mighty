fc37d164a658fb590657630cc3723ccb679d6727
Andre Biedenkapp <biedenka@informatik.uni-freiburg.de>
2021-06-04 10:58:01 +0200

    ADD first rudimentary testing for DDQN

--- test/test_agent/mock_environment.py
+++ test/test_agent/mock_environment.py
@@ -1,9 +1,20 @@
-from mighty.env.env_handling import DACENV
+import numpy as np
+from gym.spaces import Discrete, Box
 
-class MockEnv(DACENV):
+
+class MockEnvDiscreteActions:
+
+    def __init__(self):
+        self.action_space = Discrete(5)
+        self.observation_space = Box(low=np.array([0., 0.], dtype=np.float32),
+                                     high=np.array([10., 1.], dtype=np.float32),
+                                     dtype=np.float32)
+        self.__count = 0
 
     def step(self, action):
-        pass
+        self.__count += 1
+        return np.array([self.__count, 0]), 1, self.__count >= 10, None
 
     def reset(self):
-        pass
+        self.__count = 0
+        return np.array([self.__count, 0])
