9864cb0df2f60a3afe5e763f904be82d76bbe621
Theresa Eimer <eimer@tnt.uni-hannover.de>
2021-09-08 11:37:25 +0200

    Add TD3 as agent type

--- mighty/agent/factory.py
+++ mighty/agent/factory.py
@@ -1,11 +1,14 @@
 from mighty.agent.base import AbstractAgent
 from mighty.agent.ddqn import DDQNAgent
+from mighty.agent.td3 import TD3Agent
 
 
 def get_agent_class(agent_type: str) -> type(AbstractAgent):
     agent_class = None
     if agent_type == "DDQN":
         agent_class = DDQNAgent
+    elif agent_type == "TD3":
+        agent_class = TD3Agent
     else:
         raise ValueError(f"Unknown agent_type {agent_type}.")
 
