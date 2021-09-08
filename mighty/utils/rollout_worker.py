4b0aebc8c30f2644b3958555cab2691bebd33356
Theresa Eimer <eimer@tnt.uni-hannover.de>
2021-09-08 11:18:36 +0200

    MAINT

--- mighty/utils/rollout_worker.py
+++ mighty/utils/rollout_worker.py
@@ -12,7 +12,7 @@ class RolloutWorker:
             self.logger.reset_episode()
             self.logger.set_env(env)
             while not done:
-                action = self.get_action(np.array(state), epsilon=0)
+                action = self.agent.get_action(np.array(state), epsilon=0)
                 state, reward, done, _ = env.step(action)
                 avg_reward += reward
             self.logger.write()
