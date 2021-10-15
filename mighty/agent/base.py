73632b842f781fc707c9f727abfa6f2e914737e5
Theresa Eimer <eimer@tnt.uni-hannover.de>
2021-10-15 15:55:54 +0200

    log free option

--- mighty/agent/base.py
+++ mighty/agent/base.py
@@ -164,8 +164,9 @@ class AbstractAgent:
 
         """
         self.last_state = self.env.reset()
-        self.logger.reset_episode()
-        self.logger.set_train_env(self.env)
+        if self.logger is not None:
+            self.logger.reset_episode()
+            self.logger.set_train_env(self.env)
 
     def check_save_components(self):
         """
@@ -270,7 +271,7 @@ class AbstractAgent:
             env=self._env_eval,
             episodes=n_episodes_eval,
         )
-        eval_checkpoint_handler = ModelCheckpoint(self.model_dir, filename_prefix='eval_checkpoint', n_saved=None, create_dir=True)
+        eval_checkpoint_handler = ModelCheckpoint(self.model_dir, filename_prefix='eval', n_saved=None, create_dir=True)
         trainer.add_event_handler(Events.EPOCH_COMPLETED(every=eval_every_n_steps), checkpoint_metadata, agent=self,
                                   file=f"{self.model_dir}/checkpoint_list.json", checkpoint_handler=eval_checkpoint_handler, engine=trainer)
 
@@ -305,7 +306,7 @@ class AbstractAgent:
         # order of registering matters! first in, first out
         # we need to save the model first before evaluating
         trainer.add_event_handler(Events.COMPLETED, eval_checkpoint_handler, to_save=self._mapping_save_components)
-        trainer.add_event_handler(Events.COMPLETED, self.run_rollout, **eval_kwargs)
+        #trainer.add_event_handler(Events.COMPLETED, self.run_rollout, **eval_kwargs)
 
         # RUN
         iterations = range(self._max_env_time_steps)
