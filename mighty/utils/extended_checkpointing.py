f8a0c73d095e81a1fb47d00197d06076c0ec18da
Theresa Eimer <eimer@tnt.uni-hannover.de>
2021-10-15 13:48:57 +0200

    env config saving

--- mighty/utils/extended_checkpointing.py
+++ mighty/utils/extended_checkpointing.py
@@ -2,9 +2,16 @@ import json
 
 def checkpoint_metadata(agent, file, checkpoint_handler, engine):
     to_save = {}
-    #to_save["env_config"] = agent.env.config
-    to_save["timesteps"] = agent.total_steps
+    try:
+        from dacbench import AbstractBenchmark
+        bench = AbstractBenchmark()
+        bench.config = agent.env.config
+        to_save["env_config"] = bench.serialize_config()
+    except:
+        to_save["env_config"] = "Not using DACBench env, no env config saved"
 
+    to_save["timesteps"] = agent.total_steps
+    to_save["episodes"] = agent.logger.module_logger["train_performance"].episode
     to_save["model_dir"] = agent.model_dir
     to_save["checkpoint_path"] = f"{to_save['model_dir']}/model_{to_save['timesteps']/11}.pt"
 
