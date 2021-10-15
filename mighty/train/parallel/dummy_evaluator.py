62aa24a13b3fa5f88fee7e1017147de83a3762db
Carolin Benjamins <benjamins@tnt.uni-hannover.de>
2021-10-15 16:31:33 +0200

    current status

--- mighty/train/parallel/dummy_evaluator.py
+++ mighty/train/parallel/dummy_evaluator.py
@@ -1,6 +1,12 @@
+import logging
+
 import numpy as np
 from datetime import datetime
 from pathlib import Path
+import time
+import sys
+import os
+import json
 
 import torch
 import torch.nn as nn
@@ -16,13 +22,18 @@ from ignite.handlers import Checkpoint, DiskSaver, global_step_from_engine
 from ignite.metrics import Accuracy, Loss
 from ignite.utils import manual_seed, setup_logger
 
+from mighty.utils.logger import get_standard_logger
+
 
 class DummyWorker(object):
     def __init__(self):
+        # self.identifier = np.random.randint(0, 100000)
         self.var = 3
 
     def work(self, multiplicand: int):
-        return self.var * multiplicand
+        res = self.var * multiplicand
+        # print(self.identifier)
+        return res
 
 
 class DummyEvaluator(object):
@@ -34,25 +45,40 @@ class DummyEvaluator(object):
 
 
 def training(local_rank, config):
+    t_start = time.time()
     rank = idist.get_rank()
 
-    logger = setup_logger(name=f"Dummy Evaluator rank {rank}")
-    log_basic_info(logger, config)
-    if rank == 0:
-        device = idist.device()
-        backend = idist.backend()
-        world_size = idist.get_world_size()
-        logger.info(f"backend {backend}, world size {world_size}")
+    # logger = setup_logger(name=f"Dummy Evaluator rank {rank}")
+    logger = get_standard_logger(identifier=f"Dummy Evaluator rank {rank}")
+    logger.setLevel(logging.INFO)
+    # log_basic_info(logger, config)
+    # if rank == 0:
+    #     device = idist.device()
+    #     backend = idist.backend()
+    #     world_size = idist.get_world_size()
+    #     logger.info(f"backend {backend}, world size {world_size}")
+    #
+    #     if "cuda" in device.type:
+    #         config["cuda device name"] = torch.cuda.get_device_name(local_rank)
+    #         logger.info(f"cuda device name {config['cuda device name']}")
+    # logger.info(f"rank {rank}, local rank {local_rank}")
 
-        if "cuda" in device.type:
-            config["cuda device name"] = torch.cuda.get_device_name(local_rank)
-            logger.info(f"cuda device name {config['cuda device name']}")
+    # worker = config["worker"]
 
-    logger.info(f"rank {rank}, local rank {local_rank}")
-
-    worker = DummyWorker()
-    ret = worker.work(multiplicand=local_rank)
+    # ret = worker.work(multiplicand=local_rank)
+    worker_fn = config["worker_fn"]
+    ret = worker_fn(multiplicand=local_rank)
     logger.info(f"worker ret {ret}")
+
+    result = idist.all_gather(ret)
+    if local_rank == 0:
+        fname = "testresults.json"
+        # idist.barrier()
+        with open(fname, 'w') as file:
+            json.dump(result, file)
+
+    t_end = time.time()
+    print(f"{local_rank}: {t_end - t_start:.4f}s")
 
 
 def run(
@@ -83,8 +109,12 @@ def run(
 
     # idist.spawn(backend, training, args=(), kwargs_dict={"config": config}, nproc_per_node=nproc_per_node)
 
+    worker = DummyWorker()
+    config["worker_fn"] = worker.work
     with idist.Parallel(backend=backend, **spawn_kwargs) as parallel:
         parallel.run(training, config)
+    print("done")
+    idist.finalize()  # end all subprocesses
 
 
 def log_basic_info(logger, config):
@@ -115,5 +145,5 @@ def log_basic_info(logger, config):
 if __name__ == '__main__':
     run(
         backend="gloo",
-        nproc_per_node=3
+        nproc_per_node=4
     )
