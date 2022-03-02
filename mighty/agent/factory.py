9635d91e18e5d41ad84d1451768464db0337186a
Carolin Benjamins <benjamin@tnt.uni-hannover.de>
2022-03-02 07:37:32 +0100

    hydra integration part 2

--- mighty/agent/factory.py
+++ mighty/agent/factory.py
@@ -5,7 +5,7 @@ from mighty.agent.coax_ddqn import DDQNAgent
 
 def get_agent_class(agent_type: str) -> type(AbstractAgent):
     agent_class = None
-    if agent_type == "ddqn":
+    if agent_type == "DDQN":
         agent_class = DDQNAgent
     else:
         raise ValueError(f"Unknown agent_type {agent_type}.")
