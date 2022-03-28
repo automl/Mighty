ee0a037e8549ea22553264f6b31df055224d6c07
Theresa Eimer <eimer@tnt.uni-hannover.de>
2022-03-28 16:23:20 +0200

    very basic eval

--- mighty/agent/base_agent.py
+++ mighty/agent/base_agent.py
@@ -75,114 +75,115 @@ class MightyAgent(object):
             tracer_kwargs = {
                 "n": 1,
                 "gamma": 0.9,
-            }
-        self.tracer_class = tracer_class
-        self.tracer_kwargs = tracer_kwargs
+                }
+            self.tracer_class = tracer_class
+            self.tracer_kwargs = tracer_kwargs
 
-        if logger is not None:
-            output_dir = logger.log_dir
-        else:
-            output_dir = None
+            if logger is not None:
+                output_dir = logger.log_dir
+            else:
+                output_dir = None
 
-        self.env = env
-        if eval_env is None:
-            self.eval_env = self.env
-        else:
-            self.eval_env = eval_env
+            self.env = env
+            if eval_env is None:
+                self.eval_env = self.env
+            else:
+                self.eval_env = eval_env
 
-        self.logger = logger
-        self.render_progress = render_progress
-        self.output_dir = output_dir
-        if self.output_dir is not None:
-            self.model_dir = os.path.join(self.output_dir, 'models')
+            self.logger = logger
+            self.render_progress = render_progress
+            self.output_dir = output_dir
+            if self.output_dir is not None:
+                self.model_dir = os.path.join(self.output_dir, 'models')
 
-        self.last_state = None
-        self.total_steps = 0
+            self.last_state = None
+            self.total_steps = 0
 
-        self.writer = None
-        if log_tensorboard and output_dir is not None:
-            self.writer = SummaryWriter(output_dir)
-            self.writer.add_scalar('hyperparameter/learning_rate', self.learning_rate)
-            self.writer.add_scalar('hyperparameter/batch_size', self._batch_size)
-            self.writer.add_scalar('hyperparameter/policy_epsilon', self._epsilon)
+            self.writer = None
+            if log_tensorboard and output_dir is not None:
+                self.writer = SummaryWriter(output_dir)
+                self.writer.add_scalar('hyperparameter/learning_rate', self.learning_rate)
+                self.writer.add_scalar('hyperparameter/batch_size', self._batch_size)
+                self.writer.add_scalar('hyperparameter/policy_epsilon', self._epsilon)
 
-        self.initialize_agent()
+            self.initialize_agent()
 
-    def _initialize_agent(self):
-        """Agent/algorithm specific initializations."""
-        raise NotImplementedError
+        def _initialize_agent(self):
+            """Agent/algorithm specific initializations."""
+            raise NotImplementedError
 
-    def initialize_agent(self):
-        self.tracer = self.tracer_class(**self.tracer_kwargs)  # specify how to trace the transitions
-        self.replay_buffer = self.replay_buffer_class(**self.replay_buffer_kwargs)
+        def initialize_agent(self):
+            self.tracer = self.tracer_class(**self.tracer_kwargs)  # specify how to trace the transitions
+            self.replay_buffer = self.replay_buffer_class(**self.replay_buffer_kwargs)
 
-        self._initialize_agent()
+            self._initialize_agent()
 
-    def update_agent(self, step):
-        raise NotImplementedError
+        def update_agent(self, step):
+            raise NotImplementedError
 
-    def train(
-            self,
-            n_steps: int,
-            n_episodes_eval: int,
-            eval_every_n_steps: int = 1_000,
-            human_log_every_n_episodes: int = 100,
-            save_model_every_n_episodes: int = 100,
-    ):
-        step_progress = 1 / n_steps
-        episodes = 0
-        with Progress(
-                "[progress.description]{task.description}",
-                BarColumn(),
-                "[progress.percentage]{task.percentage:>3.0f}%",
-                'Remaining:',
-                TimeRemainingColumn(),
-                'Elapsed:',
-                TimeElapsedColumn()
-        ) as progress:
-            steps_task = progress.add_task("Train Steps", total=n_steps, start=False, visible=False)
-            progress.start_task(steps_task)
-            steps = 0
-            steps_since_eval = 0
-            log_reward_buffer = []
-            while steps < n_steps:
-                progress.update(steps_task, visible=True)
-                s = self.env.reset()
-                done = False
-                while not done:
-                    a = self.policy(s)
-                    s_next, r, done, info = self.env.step(a)
-                    log_reward_buffer.append(r)
-                    steps += 1
-                    progress.advance(steps_task)
+        def train(
+                self,
+                n_steps: int,
+                n_episodes_eval: int,
+                eval_every_n_steps: int = 1_000,
+                human_log_every_n_episodes: int = 100,
+                save_model_every_n_episodes: int = 100,
+        ):
+            step_progress = 1 / n_steps
+            episodes = 0
+            with Progress(
+                    "[progress.description]{task.description}",
+                    BarColumn(),
+                    "[progress.percentage]{task.percentage:>3.0f}%",
+                    'Remaining:',
+                    TimeRemainingColumn(),
+                    'Elapsed:',
+                    TimeElapsedColumn()
+            ) as progress:
+                steps_task = progress.add_task("Train Steps", total=n_steps, start=False, visible=False)
+                progress.start_task(steps_task)
+                steps = 0
+                steps_since_eval = 0
+                log_reward_buffer = []
+                while steps < n_steps:
+                    progress.update(steps_task, visible=True)
+                    s = self.env.reset()
+                    done = False
+                    while not done:
+                        a = self.policy(s)
+                        s_next, r, done, info = self.env.step(a)
+                        log_reward_buffer.append(r)
+                        steps += 1
+                        progress.advance(steps_task)
 
-                    # add transition to buffer
-                    self.tracer.add(s, a, r, done)
-                    while self.tracer:
-                        self.replay_buffer.add(self.tracer.pop())
+                        # add transition to buffer
+                        self.tracer.add(s, a, r, done)
+                        while self.tracer:
+                            self.replay_buffer.add(self.tracer.pop())
 
-                    # update
-                    if len(self.replay_buffer) >= self._batch_size:
-                        self.update_agent(steps)
+                        # update
+                        if len(self.replay_buffer) >= self._batch_size:
+                            self.update_agent(steps)
 
-                    self.last_state = s
-                    s = s_next
-                    
-                episodes += 1
+                        self.last_state = s
+                        s = s_next
+                        
+                    episodes += 1
 
-                if steps_since_eval >= eval_every_n_steps:
-                    steps_since_eval = 0
-                    self.eval(self.eval_env, n_episodes_eval)
+                    if steps_since_eval >= eval_every_n_steps:
+                        steps_since_eval = 0
+                        self.eval(self.eval_env, n_episodes_eval)
 
-                # TODO: make this more informative
-                if episodes % human_log_every_n_episodes == 0:
-                    print(f"Steps: {steps}, Reward: {sum(log_reward_buffer) / len(log_reward_buffer)}")
-                    log_reward_buffer = []
+                    # TODO: make this more informative
+                    if episodes % human_log_every_n_episodes == 0:
+                        print(f"Steps: {steps}, Reward: {sum(log_reward_buffer) / len(log_reward_buffer)}")
+                        log_reward_buffer = []
 
-                if episodes % save_model_every_n_episodes == 0:
-                    self.save()
+                    if episodes % save_model_every_n_episodes == 0:
+                        self.save()
 
-    def run(
+        #FIXME: should be removed once run_mighty is remodelled
+        def run(
             self,
             n_steps: int,
             n_episodes_eval: int,
@@ -228,4 +229,9 @@ class MightyAgent(object):
         :param episodes:
         :return:
         """
-        raise NotImplementedError
+        for e in range(episodes):
+            done = False
+            state = env.reset()
+            while not done:
+                action = self.policy(state)
+                state, reward, done, _ = env.step(action)
