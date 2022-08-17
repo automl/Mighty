7c527dd125d62ad234088da35bd09142adf56551
André Biedenkapp <andreh.biedenka@gmx.de>
2022-08-17 16:21:07 +0200

    FIX failing test due to unset output_path in logger

--- test/test_logging.py
+++ test/test_logging.py
@@ -10,12 +10,14 @@ class TestBaseAgent(unittest.TestCase):
         env = MockEnvDiscreteActions()
         logger = Logger(
             experiment_name=f"test",
+            output_path='.',
             step_write_frequency=10,
             episode_write_frequency=None,
             log_to_wandb=False,
             log_to_tensorboad=False,
             hydra_config=None
         )
+        self.log_file_path = logger.log_dir
 
         self.assertTrue(logger.instance is None)
         self.assertFalse(logger.log_file is None)
@@ -36,6 +38,11 @@ class TestBaseAgent(unittest.TestCase):
         )
 
         self.assertFalse(self.agent.logger is None)
+
+    def tearDown(self) -> None:
+        import os
+        os.remove(os.path.join(self.log_file_path, 'eval.jsonl'))
+        os.remove(os.path.join(self.log_file_path, 'rewards.jsonl'))
 
     def testEvalSwitch(self):
         self.assertFalse(self.agent.logger.eval)
