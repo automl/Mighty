e310e36ee97812713cfdd185c92dc918631bf677
Carolin Benjamins <c.benjamins@ai.uni-hannover.de>
2023-12-19 11:19:14 +0100

    Adjust path

--- mighty/tune_mighty.sh
+++ mighty/tune_mighty.sh
@@ -1,19 +1,19 @@
 # TODO: Can we sweep over envs here? Do we want to?
 #DQN on classic control
-python run_mighty.py -m env=CartPole-v1 +search_space=dqn_gym_classic algorithm=dqn 'num_steps=25e4' --config-name smac
-python run_mighty.py -m env=MountainCar-v0 +search_space=dqn_gym_classic algorithm=dqn 'num_steps=25e4' --config-name smac
+python mighty/run_mighty.py -m env=CartPole-v1 +search_space=dqn_gym_classic algorithm=dqn 'num_steps=25e4' --config-name smac
+python mighty/run_mighty.py -m env=MountainCar-v0 +search_space=dqn_gym_classic algorithm=dqn 'num_steps=25e4' --config-name smac
 
 #SAC on classic control
-python run_mighty.py -m env=LunarLander-v2 +search_space=sac_gym_classic algorithm=sac env_kwargs={'continuous': true} 'num_steps=25e4' --config-name smac
-python run_mighty.py -m env=MountainCarContinuous-v0 +search_space=sac_gym_classic algorithm=sac 'num_steps=25e4' --config-name smac
-python run_mighty.py -m env=Pendulum-v1 +search_space=sac_gym_classic algorithm=sac 'num_steps=25e4' --config-name smac
+python mighty/run_mighty.py -m env=LunarLander-v2 +search_space=sac_gym_classic algorithm=sac env_kwargs={'continuous': true} 'num_steps=25e4' --config-name smac
+python mighty/run_mighty.py -m env=MountainCarContinuous-v0 +search_space=sac_gym_classic algorithm=sac 'num_steps=25e4' --config-name smac
+python mighty/run_mighty.py -m env=Pendulum-v1 +search_space=sac_gym_classic algorithm=sac 'num_steps=25e4' --config-name smac
 
 #PPO on classic control
-python run_mighty.py -m env=LunarLander-v2 +search_space=ppo_gym_classic algorithm=ppo env_kwargs={'continuous': true} 'num_steps=25e4' --config-name smac
-python run_mighty.py -m env=MountainCarContinuous-v0 +search_space=ppo_gym_classic algorithm=ppo 'num_steps=25e4' --config-name smac
-python run_mighty.py -m env=Pendulum-v1 +search_space=ppo_gym_classic algorithm=ppo 'num_steps=25e4' --config-name smac
+python mighty/run_mighty.py -m env=LunarLander-v2 +search_space=ppo_gym_classic algorithm=ppo env_kwargs={'continuous': true} 'num_steps=25e4' --config-name smac
+python mighty/run_mighty.py -m env=MountainCarContinuous-v0 +search_space=ppo_gym_classic algorithm=ppo 'num_steps=25e4' --config-name smac
+python mighty/run_mighty.py -m env=Pendulum-v1 +search_space=ppo_gym_classic algorithm=ppo 'num_steps=25e4' --config-name smac
 
 #DQN on DACBench
-python run_mighty.py -m env=SigmoidBenchmark env_wrappers=[dacbench.wrappers.MultiDiscreteWrapper] +search_space=dqn_dacbench_toy algorithm=dqn env_kwargs={"benchmark": True, "dimension": 1} 'num_steps=25e4' --config-name smac
-python run_mighty.py -m env=SigmoidBenchmark env_wrappers=[dacbench.wrappers.MultiDiscreteWrapper] +search_space=dqn_dacbench_toy algorithm=dqn env_kwargs={"benchmark": True, "dimension": 5} 'num_steps=25e4' --config-name smac
-python run_mighty.py -m env=FastDownwardBenchmark +search_space=dqn_dacbench_fd algorithm=dqn 'num_steps=25e4' --config-name smac
+python mighty/run_mighty.py -m env=SigmoidBenchmark env_wrappers=[dacbench.wrappers.MultiDiscreteWrapper] +search_space=dqn_dacbench_toy algorithm=dqn env_kwargs={"benchmark": True, "dimension": 1} 'num_steps=25e4' --config-name smac
+python mighty/run_mighty.py -m env=SigmoidBenchmark env_wrappers=[dacbench.wrappers.MultiDiscreteWrapper] +search_space=dqn_dacbench_toy algorithm=dqn env_kwargs={"benchmark": True, "dimension": 5} 'num_steps=25e4' --config-name smac
+python mighty/run_mighty.py -m env=FastDownwardBenchmark +search_space=dqn_dacbench_fd algorithm=dqn 'num_steps=25e4' --config-name smac
